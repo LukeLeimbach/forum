66bf6c36f13a8eed70f21db21f59fc259e64ec52
Xan Frank <slapmymemedaddy420@gmail.com>
2022-10-03 23:14:45 -0600

    test cases

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-batja#72(2*w^7$d@ad0w_*@acntc^s^59vw=k7hkk(ws*^e_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition

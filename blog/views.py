efea374db18c31e67b7763d3b893a98b6dd09450
Luke Leimbach <luke.leimbach@gmail.com>
2022-10-06 11:18:25 -0600

    Rewrite from book

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 from .models import Post
 
-class HomePageView(ListView):
+
+class BlogListView(ListView):
     model = Post
     template_name = "home.html"

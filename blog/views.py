75c30601f0a8428933fbf4264984f2daaf7ed0f6
Xan Frank <slapmymemedaddy420@gmail.com>
2022-10-10 19:06:31 -0600

    ch.6 tests and login/logout

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
 
@@ -18,5 +20,10 @@ class BlogCreateView(CreateView):
 
 class BlogUpdateView(UpdateView):
     model = Post
-    template_name = "post_new"
+    template_name = "post_edit.html"
     fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
